b86353d7c2a582a355ca10e23da4cb771be18be1
JHANAVI J S <jhanavijs02@gmail.com>
2025-04-01 18:45:40 +0530

    commit 4

--- B09/src/com/tns/TicketBooking/TicketBooking.java
+++ B09/src/com/tns/TicketBooking/TicketBooking.java
@@ -1,10 +1,23 @@
 package com.tns.TicketBooking;
 
-public class TicketBooking {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+class TicketSystem {
+    private static int ticket = 5;
+    
+    public static void bookTicket(String name) {
+        if (ticket > 0) {
+            System.out.println(name + " has booked the ticket. Ticket number: " + ticket);
+            ticket--;
+        } else {
+            System.out.println("Sorry, " + name + ", no tickets available.");
+        }
+    }
+    
+    public static void displayTickets() {
+        if (ticket > 0) {
+            System.out.println("Tickets available: " + ticket);
+        } else {
+            System.out.println("All tickets are booked.");
+        }
+    }
 }
+
