a7469419d6a29508e1e3d5bfd6f0b6293b7a9752
JHANAVI J S <jhanavijs02@gmail.com>
2025-04-02 00:48:12 +0530

    commit 6

--- TNSIF_HOMEWORK/src/com/tnsif/TicketBookingSystem/DemoBooking.java
+++ TNSIF_HOMEWORK/src/com/tnsif/TicketBookingSystem/DemoBooking.java
@@ -4,7 +4,7 @@ public class DemoBooking {
     public static void main(String[] args) {
         TicketBookingSystem ob = new TicketBookingSystem();
         
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
             System.out.println("Person " + i + " is booking the ticket...");
             ob.bookTicket();
         }
