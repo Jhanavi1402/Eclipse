c5ce0d6955a62d8a0abebcb9542f35427486a302
JHANAVI J S <jhanavijs02@gmail.com>
2025-04-09 17:50:30 +0530

    commit 10

---
+++ B09/src/com/tnsif/nonaccess/addition.java
@@ -0,0 +1,5 @@
+package com.tnsif.nonaccess;
+
+public class addition {
+
+}
--- B09/src/com/tnsif/nonaccess/demo.java
+++ B09/src/com/tnsif/nonaccess/demo.java
@@ -1,4 +1,4 @@
-package com.tns.nonaccess;
+package com.tnsif.nonaccess;
 
 public class demo {
 	public static void main(String[] args) {
