9b33b5ec99d166fbe122114afd0ed919548f6a77
JHANAVI J S <jhanavijs02@gmail.com>
2025-04-24 18:05:31 +0530

    commit 17

--- B09/src/com/tnsif/JDBC/JdbcDemo.java
+++ B09/src/com/tnsif/JDBC/JdbcDemo.java
@@ -1,12 +1,46 @@
 package com.tnsif.JDBC;
+import java.sql.*;//2nd step
 
 public class JdbcDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String url = "jdbc:postgresql://localhost:5432/employee";
-		String username="postgres";
-		String password = "5342";
+		String url = "jdbc:postgresql://localhost:5432/JDBCDEMO";//db url
+		String username="postgres";//username of db
+		String password = "1497";//db password
+		
+		try {
+			//load the driver - 3rd step
+			Class.forName("org.postgresql.Driver");
+			
+			//Establishing the connection - 4th step
+			//connection interface
+			Connection con=DriverManager.getConnection(url,username,password);
+			
+			//Define  SQL Query -5th step
+			Statement st= con.createStatement();
+			
+			//executing the query -6th step
+			String query = "SELECT * FROM employee";
+			
+			//Process the Result - 7th step
+			ResultSet rs= st.executeQuery(query);
+			
+			while(rs.next())
+			{
+				String table =rs.getString(1) + ":"+ rs.getInt(2);
+				System.out.println(table);
+			}
+			
+			//close the resources -8th
+			rs.close();
+			st.close();
+			con.close();
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
-
 }
---
+++ B09/src/stream_api/StreamApiDemo.java
@@ -0,0 +1,19 @@
+package stream_api;
+import java.util.*;
+import java.util.stream.Collectors;
+//import java.util.stream.*; can import it this way too
+
+public class StreamApiDemo {
+
+	public static void main(String[] args) {
+		//asList converts array into list
+		List<String> names = Arrays.asList("Hello","World","Alice","","Jhanavi","Katharine","Sandhya","Kedhar","Luxmi");
+		List<String> result = names.stream().filter(name -> name.startsWith("A"))
+                							.map(String::toUpperCase)
+                							.collect(Collectors.toList());
+		System.out.println("Filtered Names: "+result);
+
+
+	}
+
+}
